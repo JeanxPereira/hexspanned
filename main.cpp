1e736a2efd033d2cd292dd228babbc0409cb2e78
zyntaks <soaproap@gmail.com>
2024-07-20 04:22:31 -0400

    Separate some things from main

--- main.cpp
+++ main.cpp
@@ -11,24 +11,24 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-MemoryEditor memEdit;
-ImGui::FileBrowser fileDialog;
-std::vector<uint8_t> data;
-unsigned vao, vbo;
-glm::mat4 projection, view, model;
-
-int vertex_buffer_start = 0;
-int vertex_count = 3;
-int vertex_stride = 12;
-bool bigEndian = true;
-bool wireframe = false;
-bool backfaceCulling = false;
-float viewDistance = 3.0f;
-bool indexedDraw = false;
-int indices_start;
-
-void copyToGPU()
+struct VisParams
 {
+    int vertexBufferStart = 0;
+    int indexBufferStart = 0;
+    int vertexCount = 3;
+    int vertexStride = 12;
+    bool bigEndian = true;
+    bool wireframe = false;
+    bool backfaceCulling = false;
+    float viewDistance = 3.0f;
+    bool indexedDraw = false;
+    bool halfWidthIndexes = false;
+};
+
+void copyToGPU(unsigned vbo, std::vector<uint8_t>& data, bool& bigEndian)
+{
+
+    // TODO: Endian swap should take into account offset, probably requiring a re-upload with each address change
     std::vector<uint8_t> endian_swapped;
     std::vector<uint8_t> *pData = &data;
     if (bigEndian) {
@@ -46,14 +46,14 @@ void copyToGPU()
 
     // Update buffer bindings
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, pData->size(), pData->data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, (long) pData->size(), pData->data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 unsigned compile_shader(const char *source, unsigned type)
 {
     unsigned shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
+    glShaderSource(shader, 1, &source, nullptr);
     glCompileShader(shader);
 
     int status;
@@ -63,6 +63,52 @@ unsigned compile_shader(const char *source, unsigned type)
     }
 
     return shader;
+}
+
+bool
+drawVisMenu(VisParams& visParams, int editAddress)
+{
+    bool needsReupload = false;
+
+    ImGui::Begin("Vertex Visualization");
+    ImGui::InputInt("Start", &visParams.vertexBufferStart, 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+    if (ImGui::Button("Set to Highlighted Address")) {
+        visParams.vertexBufferStart = editAddress;
+    }
+    
+    if (visParams.indexedDraw) {
+        ImGui::InputInt("Index Start", &visParams.indexBufferStart, 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+        if (ImGui::Button("Set to Highlighted Address#STHA_IND")) {
+            visParams.indexBufferStart = editAddress;
+        }
+    }
+
+    ImGui::InputInt("Count", &visParams.vertexCount, 1, 100, 0);
+    ImGui::InputInt("Stride", &visParams.vertexStride, 1, 100, 0);
+    ImGui::Checkbox("Indexed Draw", &visParams.indexedDraw);
+
+    if (visParams.indexedDraw) {
+        ImGui::Checkbox("Half-Width (16-bit) Indexes", &visParams.halfWidthIndexes);
+    }
+
+    if (ImGui::Checkbox("Big-Endian", &visParams.bigEndian)) {
+        needsReupload = true;
+    }
+
+    if (ImGui::Checkbox("Wireframe", &visParams.wireframe)) {
+        glPolygonMode(GL_FRONT_AND_BACK, visParams.wireframe ? GL_LINE : GL_FILL);
+    }
+
+    if (ImGui::Checkbox("Backface Culling", &visParams.backfaceCulling)) {
+        if (visParams.backfaceCulling)
+            glEnable(GL_CULL_FACE);
+        else
+            glDisable(GL_CULL_FACE);
+    }
+    ImGui::InputFloat("View Distance", &visParams.viewDistance);
+    ImGui::End();
+
+    return needsReupload;
 }
 
 int main()
@@ -75,7 +121,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "hexspanned", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "hexspanned", nullptr, nullptr);
 
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
@@ -84,11 +130,14 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330 core");
 
+    MemoryEditor memEdit;
+    ImGui::FileBrowser fileDialog;
+    std::vector<uint8_t> data;
+    unsigned vao, vbo;
+    VisParams visParams;
+
     glGenVertexArrays(1, &vao);
     glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBindVertexArray(vao);
-
     glEnable(GL_DEPTH_TEST);
 
     unsigned vertex_shader = compile_shader(
@@ -122,42 +171,9 @@ int main()
         }
         ImGui::EndMainMenuBar();
 
-        ImGui::Begin("Vertex Visualization");
-        ImGui::InputInt("Start", &vertex_buffer_start, 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
-        if (ImGui::Button("Set to Highlighted Address")) {
-            vertex_buffer_start = memEdit.DataEditingAddr;
+        if (drawVisMenu(visParams, (int) memEdit.DataEditingAddr)) {
+            copyToGPU(vbo, data, visParams.bigEndian);
         }
-        if (indexedDraw) {
-            ImGui::InputInt("Index Start", &indices_start, 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
-            if (ImGui::Button("Set to Highlighted Address#STHA_IND")) {
-                indices_start = memEdit.DataEditingAddr;
-            }
-        }
-
-        ImGui::InputInt("Count", &vertex_count, 1, 100, 0);
-        ImGui::InputInt("Stride", &vertex_stride, 1, 100, 0);
-
-        ImGui::Checkbox("Indexed Draw", &indexedDraw);
-
-
-        if (ImGui::Checkbox("Big-Endian", &bigEndian)) {
-            copyToGPU();
-        }
-        if (ImGui::Checkbox("Wireframe", &wireframe)) {
-            if (wireframe) glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-            else
-                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-        }
-        if (ImGui::Checkbox("Backface Culling", &backfaceCulling)) {
-            if (backfaceCulling) {
-                glEnable(GL_CULL_FACE);
-                glCullFace(GL_BACK);
-            } else {
-                glDisable(GL_CULL_FACE);
-            }
-        }
-        ImGui::InputFloat("View Distance", &viewDistance);
-        ImGui::End();
 
         memEdit.DrawWindow("Hex View", data.data(), data.size());
 
@@ -172,8 +188,7 @@ int main()
             in.read((char *) data.data(), size);
             in.close();
 
-            copyToGPU();
-
+            copyToGPU(vbo, data, visParams.bigEndian);
             fileDialog.Close();
         }
 
@@ -182,28 +197,32 @@ int main()
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertex_stride, (void *) (uintptr_t) vertex_buffer_start);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, visParams.vertexStride,
+                              (void *) (uintptr_t) visParams.vertexBufferStart);
         glEnableVertexAttribArray(0);
 
         glUseProgram(program);
 
-
-        projection = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 1000.0f);
-        view = glm::lookAt(glm::vec3(1, 1, 1) * viewDistance, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-        model = glm::identity<glm::mat4>();
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 1280.0f / 720.0f, 0.1f, 1000.0f);
+        glm::mat4 view = glm::lookAt(glm::vec3(1, 1, 1) * visParams.viewDistance, glm::vec3(0, 0, 0),
+                                     glm::vec3(0, 1, 0));
+        auto model = glm::identity<glm::mat4>();
         glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
-        if ((indexedDraw && vertex_count * 4 + indices_start < data.size()) ||
-            (!indexedDraw && vertex_stride * vertex_count + vertex_buffer_start < data.size())) {
-            if (indexedDraw) {
-                glDrawElements(GL_TRIANGLES, vertex_count, GL_UNSIGNED_INT, ((void *) (uintptr_t) indices_start));
+        if ((visParams.indexedDraw && visParams.vertexCount * 4 + visParams.indexBufferStart < data.size()) ||
+            (!visParams.indexedDraw &&
+             visParams.vertexStride * visParams.vertexCount + visParams.vertexBufferStart < data.size())) {
+            if (visParams.indexedDraw) {
+                glDrawElements(GL_TRIANGLES, visParams.vertexCount,
+                               visParams.halfWidthIndexes ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT,
+                               ((void *) (uintptr_t) visParams.indexBufferStart));
             } else {
-                glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+                glDrawArrays(GL_TRIANGLES, 0, visParams.vertexCount);
             }
         } else {
-            ImGui::Begin("Oops!", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+            ImGui::Begin("Oops!", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
             ImGui::Text("The current render parameters would read past the end of the file!");
             ImGui::End();
         }
