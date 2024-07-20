8ea192c240276c3161a819c96b157510b6d9100c
zyntaks <soaproap@gmail.com>
2024-07-20 02:20:23 -0400

    Add indexed drawing

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@ bool bigEndian = true;
 bool wireframe = false;
 bool backfaceCulling = false;
 float viewDistance = 3.0f;
+bool indexedDraw = false;
+int indices_start;
 
 void copyToGPU()
 {
@@ -125,8 +127,19 @@ int main()
         if (ImGui::Button("Set to Highlighted Address")) {
             vertex_buffer_start = memEdit.DataEditingAddr;
         }
+        if (indexedDraw) {
+            ImGui::InputInt("Index Start", &indices_start, 1, 100, ImGuiInputTextFlags_CharsHexadecimal);
+            if (ImGui::Button("Set to Highlighted Address#STHA_IND")) {
+                indices_start = memEdit.DataEditingAddr;
+            }
+        }
+
         ImGui::InputInt("Count", &vertex_count, 1, 100, 0);
         ImGui::InputInt("Stride", &vertex_stride, 1, 100, 0);
+
+        ImGui::Checkbox("Indexed Draw", &indexedDraw);
+
+
         if (ImGui::Checkbox("Big-Endian", &bigEndian)) {
             copyToGPU();
         }
@@ -168,6 +181,7 @@ int main()
 
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertex_stride, (void *) (uintptr_t) vertex_buffer_start);
         glEnableVertexAttribArray(0);
 
@@ -181,8 +195,13 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, glm::value_ptr(model));
 
-        if (vertex_stride * vertex_count + vertex_buffer_start < data.size()) {
-            glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+        if ((indexedDraw && vertex_count * 4 + indices_start < data.size()) ||
+            (!indexedDraw && vertex_stride * vertex_count + vertex_buffer_start < data.size())) {
+            if (indexedDraw) {
+                glDrawElements(GL_TRIANGLES, vertex_count, GL_UNSIGNED_INT, ((void *) (uintptr_t) indices_start));
+            } else {
+                glDrawArrays(GL_TRIANGLES, 0, vertex_count);
+            }
         } else {
             ImGui::Begin("Oops!", NULL, ImGuiWindowFlags_AlwaysAutoResize);
             ImGui::Text("The current render parameters would read past the end of the file!");
